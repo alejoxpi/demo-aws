28b8a2a78ca14c0dc9a267cf049bfbaf4b0b7a19
Alejandro Benitez <alejoxpi@gmail.com>
2021-02-19 21:09:15 -0500

    Se actualiza

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,12 +11,12 @@ public class HelloController {
 	
 	@GetMapping
 	public String index() {
-		return "Hello from Spring Boot and AWS";
+		return "Hola from Spring Boot and AWS";
 	}
 	
 	@GetMapping("/{name}")
 	public String index(@PathVariable("name") String name) {
-		return "Hello "+name+" from Spring Boot and AWS";
+		return "Hola "+name+" from Spring Boot and AWS";
 	}
 
 }
